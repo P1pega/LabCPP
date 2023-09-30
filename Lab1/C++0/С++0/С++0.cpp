3accdabd0b8ff3e9e0416f29b086bb7fe5f67697
P1pega <146541895+P1pega@users.noreply.github.com>
2023-09-30 19:40:43 +0300

    Add files via upload

---
+++ Lab1/C++0/С++0/С++0.cpp
@@ -0,0 +1,11 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    cout << "Hello World!\n"<< endl << "Привет мир!";
+    return 0;
+}
+
---
+++ Lab1/C++1/C++/C++.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Введите два числа:";
+		cin >> a >> b;
+	c = a + b;
+	cout <<"Сумма чисел:"<< c;
+	return 0;
+
+
+}
